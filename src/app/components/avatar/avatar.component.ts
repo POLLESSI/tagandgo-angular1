19ebc83ff3829d4497f3ecd59ae3ee4825af81d2
Maxime-Spark <maxforain@gmail.com>
2024-08-30 11:44:26 +0200

    fix avatar

--- src/app/components/avatar/avatar.component.ts
+++ src/app/components/avatar/avatar.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Form } from '@angular/forms';
-//import * as signalr from '@microsoft/signalr';
 import { AvatarModel } from 'src/app/models/avatar.model';
 import { AvatarService } from 'src/app/services/avatar.service';
 
@@ -10,15 +8,13 @@ import { AvatarService } from 'src/app/services/avatar.service';
   styleUrl: './avatar.component.css'
 })
 export class AvatarComponent implements OnInit {
+
   listAvatars: AvatarModel[] = [];
-  //listAvatar: Array<AvatarModel> [];
 
   avatarName! : string;
   avatarUrl! : string;
   description! : string;
   avatar_Id! : number;
-
-  //hubConnection! : signalr.HubConnection;
 
   disable! : boolean;
 
@@ -30,7 +26,7 @@ export class AvatarComponent implements OnInit {
 
   async getAllAvatars(): Promise<void> {
     try {
-      this.listAvatars = await this.avatarService['getAllAvatars']();
+      this.listAvatars = await this.avatarService.getAllAvatars();
 
     } catch (error) {
       console.log("Error list avatars");
@@ -44,44 +40,7 @@ export class AvatarComponent implements OnInit {
       description: this.description,
       avatar_Id: this.avatar_Id
     };
-    // this.hubConnection.invoke("SubmitAvatar", avatar)
-    //   .catch(err => console.error(err));
+
   }
 
 }
-
-// deleteAvatar(avatar_Id: number) {
-//   this.hubConnection.invoke("DeleteAvatar", avatar_Id)
-//     .catch(err => console.error(err));
-// }
-// getAllAvatar() {
-//   this.hubConnection.invoke("GetAllAvatars")
-//     .catch(err => console.error(err));
-// }
-
-// getAvatar(avatar_Id: number) {
-//   this.hubConnection.invoke("GetAvatar", avatar_Id)
-//     .then(avatar => console.log(avatar))
-//     .catch(err => console.error(err));
-// }
-
-// this.hubConnection = new signalr.HubConnectionBuilder()
-// .withUrl("https://localhost:7069/avatar")
-// .build();
-
-// this.hubConnection.on("receiveAvatar",
-// (avatar : Avatar) => {
-// this.ListAvatar.push(avatar);
-// });
-
-// // this.hubConnection.on("ReceiveAllAvatars", (avatars: Avatar[]) => {
-// //   this.ListAvatar = avatars;
-// // });
-
-// // this.hubConnection.on("AvatarDeleted", (avatar_Id: number) => {
-// //   this.ListAvatar = this.ListAvatar.filter(av => av.avatar_Id !== avatar_Id);
-// // });
-
-// this.hubConnection.start()
-// .then(() => console.log("Connected Rigth !!!!"))
-// .catch((error) => console.log(error))
